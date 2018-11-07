60d7a906c95aaf41226bd3fc3b9c7a5fddfb7ddb
Jaryd <jarydchandos.5@gmail.com>
2018-11-07 12:46:42 -0800

    Jaryd

--- webapp.py
+++ webapp.py
@@ -6,27 +6,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_main():
-    render_template('index.html', my_variable = get_state_options())
+    return render_template('index.html', my_variable = get_state_options())
 
 def get_state_options():
-        listOfStates = []
-        with open('countydemographics.JSON') as demographics_data:
-            counties = json.load(demographics_data)
-        state  = county["State"]
-        #First step is to create a list of all states
-        for county in counties:
-            state == county["State"]
-            TrueFalse = state in listOfStates
-            if (TrueFalse == False):
-                listOfStates.append()
-
-                #add the county's state to listOfStates
-        #Second step is to create a string containing html
-        #code for the options in the select element
-        #String options is initialized to the empty String
-        #for each state in listOfStates
-            #options = options + Markup("<option value=\""+s+"\">"+s+"</option>")
-        #return options
+    listOfStates = []
+    with open('countydemographics.JSON') as demographics_data:
+        counties = json.load(demographics_data)
+    #state  = county["State"]
+    #First step is to create a list of all states
+    for county in counties:
+        state = county["State"]
+        if not state in listOfStates :
+            listOfStates.append(state)
+    print(listOfStates)
+    return listOfStates
+            #add the county's state to listOfStates
+    #Second step is to create a string containing html
+    #code for the options in the select element
+    #String options is initialized to the empty String
+    #for each state in listOfStates
+        #options = options + Markup("<option value=\""+s+"\">"+s+"</option>")
+    #return options
 
 if __name__ == "__main__":
     app.run(debug=True)
