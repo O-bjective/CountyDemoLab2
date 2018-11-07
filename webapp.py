49b46915c3397e58704d2b4c30250e5aad2f4772
Jaryd <jarydchandos.5@gmail.com>
2018-11-07 12:30:19 -0800

    Jaryd

--- webapp.py
+++ webapp.py
@@ -2,10 +2,7 @@ from flask import Flask, request, Markup, render_template, flash
 import os
 import json
 
-app = Flask(_name_)
-
-{{my_variable}}
-#render_template('index.html', my_variable="state")
+app = Flask(__name__)
 
 @app.route("/")
 def render_main():
@@ -13,14 +10,23 @@ def render_main():
 
 def get_state_options():
         listOfStates = []
+        with open('countydemographics.JSON') as demographics_data:
+            counties = json.load(demographics_data)
+        state  = county["State"]
         #First step is to create a list of all states
         for county in counties:
-            if county["State"] in listOfStates:
+            state == county["State"]
+            TrueFalse = state in listOfStates
+            if (TrueFalse == False):
+                listOfStates.append()
 
-                add the county's state to listOfStates
+                #add the county's state to listOfStates
         #Second step is to create a string containing html
         #code for the options in the select element
-        String options is initialized to the empty String
-        for each state in listOfStates
-            options = options + Markup("<option value=\""+s+"\">"+s+"</option>")
-        return options
+        #String options is initialized to the empty String
+        #for each state in listOfStates
+            #options = options + Markup("<option value=\""+s+"\">"+s+"</option>")
+        #return options
+
+if __name__ == "__main__":
+    app.run(debug=True)
